5e0f2709df58411ce8ea35a8d7c989d8e1312d99
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-23 02:18:43 +0500

    Add project files.

---
+++ Tyuiu.SizikovSS.Sprint6.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SizikovSS.Sprint6.Task0.V22.Lib;
+
+namespace Tyuiu.SizikovSS.Sprint6.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            Assert.AreEqual(11.68, ds.Calculate(2));
+        }
+    }
+}
